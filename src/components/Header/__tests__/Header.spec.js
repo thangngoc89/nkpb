d6da4454f326f9fe6e0be5155b8e79d166bc62f1
Khoa Nguyen <hi@khoanguyen.me>
2016-08-29 09:35:34 +0700

    LGTM

--- src/components/Header/__tests__/Header.spec.js
+++ src/components/Header/__tests__/Header.spec.js
@@ -2,10 +2,21 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Header from '../Header'
 
+const noop = () => {}
+const props = {
+  isMaximized: false,
+  isMinimized: false,
+  close: noop,
+  maximize: noop,
+  unmaximize: noop,
+  minimize: noop,
+  restore: noop,
+}
+
 describe('Header', () => {
   it('render with default props', () => {
     const component = renderer.create(
-      <Header />
+      <Header {...props} />
     )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
@@ -13,7 +24,7 @@ describe('Header', () => {
 
   it('render with custom backgrund and theme', () => {
     const component = renderer.create(
-      <Header background="black" theme="dark" />
+      <Header background="black" theme="dark" {...props} />
     )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
